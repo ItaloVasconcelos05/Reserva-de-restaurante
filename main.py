ea4aa92a909630abbf166e36109696b9aabd22c6
Giulliano Muniz <163376922+giulms@users.noreply.github.com>
2024-05-31 20:03:44 -0300

    Bug da atualização

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def abrir_Restaurante():
      with open('restaurantes.json', 'r') as arquivo:
         restaurantes = json.load(arquivo) 
         return restaurantes
+     
+def abrir_Reserva():
+     with open('reserva.json', 'r') as arquivo:
+        reservas = json.load(arquivo) 
+        return reservas
 
 def menu_Inicial():
     print('|  Bem vindo ao Super CRUD 🦸🏽‍♂️  |')
@@ -23,23 +28,31 @@ def menu_Cliente():
     print('2. Visualize sua reserva')
     print('3. Sair do Sistema')
 
-def pesquisar_Restaurante(nome):
-    with open('restaurantes.json','r') as arquivo:
-       restaurantes = json.load(arquivo)  
-    encontrado = False
-
-    for procurar_nome in restaurantes:
-       if procurar_nome['nome'] == nome:
-            print(f"Nome do restaurante:{procurar_nome['nome']}, \nCozinha:{procurar_nome['cozinha']}, \nFuncionamento:{procurar_nome['funcionamento']}")
-            encontrado = True
-    if not encontrado:
-            print ('Usuário não cadastrado.')    
+def escolhaRestaurante():
+    limpa()
+    print('Escolha o restaurante')
+    listaDeRestaurantes()
+    resposta = int(input('\n'))
+    limpa()
+    restaurante = abrir_Restaurante()
+    print(f"{restaurante[resposta-1]['nome']}")
+    print(f"Cozinha: {restaurante[resposta-1]['cozinha']}")
+    print(f"Horário de Funcionamento: {restaurante[resposta-1]['funcionamento']}\n")
+    print('1. Fazer Cadastro')
+    print('2. Voltar')
+    return resposta
 
 def listaDeRestaurantes():
     restaurantes = abrir_Restaurante()
     print("|  Restaurantes  |")
     for indice, restaurante in enumerate(restaurantes, start=1):
         print (f"{indice}. {restaurante['nome']}")
+
+def listaDeReservas():
+    reservas = abrir_Reserva()
+    print("|  Reservas  |")
+    for indice, reserva in enumerate(reservas, start=1):
+        print (f"{indice}. {reserva['restauranteCad']}")
 
 def fazerCadastro(restauranteCad, nomeReserva, data, horario, quantPessoas):
     with open('reserva.json', 'r') as arquivo:
@@ -49,7 +62,43 @@ def fazerCadastro(restauranteCad, nomeReserva, data, horario, quantPessoas):
 
     with open('reserva.json', 'w') as arquivo:
         json.dump(reserva, arquivo, indent=4)
-    print("😎 USUÁRIO ADICIONADO COM SUCESSO!")
+    print("\n😎 USUÁRIO ADICIONADO COM SUCESSO!")
+    sleep(2)
+    limpa()
+def atualizarReserva(restauranteAntigo,nomeAntigo,novoRestaurante,novoNome,novaData,novoHorario,novaQuantPessoas):
+    with open('reserva.json', 'r') as arquivo:
+        reserva = json.load(arquivo)
+    
+    for reservas in reserva:
+        if reservas['restauranteCad'] == restauranteAntigo and reservas['nomeReserva'] == nomeAntigo:
+           reservas['restauranteCad'] = novoRestaurante
+           reservas['nomeReserva'] = novoNome
+           reservas['data'] = novaData
+           reservas['horario'] = novoHorario
+           reservas['quantPessoas'] = novaQuantPessoas
+           break
+        else:
+            print("Essa reserva não existe =(")
+    with open('reserva.json', 'w') as arquivo:
+        json.dump(reserva, arquivo, indent=4)
+    print("😎 RESERVA ATUALIZADA COM SUCESSO!")
+
+def pesquisaRestaurante():
+    restaurante = abrir_Restaurante()
+    resposta = escolhaRestaurante()
+    resposta2 = int(input(''))
+    limpa()
+
+    if resposta2 == 1:
+        restauranteCad = restaurante[resposta-1]['nome']
+        nomeReserva = input('Digite seu nome:\n')
+        data = input('Data:\n')
+        horario = input('Horário:\n')
+        quantPessoas = input('Quantidade de Pessoas:\n')
+        fazerCadastro(restauranteCad, nomeReserva, data, horario, quantPessoas)
+
+    elif resposta2 == 2:
+        pesquisaRestaurante()
 
 def limpa():
     os.system('cls')
@@ -75,28 +124,30 @@ def main():
                 match (resposta):
                     
                     case 1:
+                        pesquisaRestaurante()
+                        sleep(2)
+                        main()
+
+                    case 2:
                         limpa()
-                        print('Escolha o restaurante')
-                        listaDeRestaurantes()
-                        resposta = int(input('\n'))
-                        limpa()
-                        restaurante = abrir_Restaurante()
-                        print(f"{restaurante[resposta-1]['nome']}")
-                        print(f"Cozinha: {restaurante[resposta-1]['cozinha']}")
-                        print(f"Horário de Funcionamento: {restaurante[resposta-1]['funcionamento']}\n")
-                        print('1. Fazer Cadastro')
-                        print('2. Voltar')
+                        listaDeReservas()
                         resposta2 = int(input(''))
                         limpa()
-                        if resposta2 == 1:
-                            restauranteCad = restaurante[resposta-1]['nome']
-                            nomeReserva = input('Digite seu nome').upper
-                            data = input('Data')
-                            horario = input('Horário')
-                            quantPessoas = input('Quantidade de Pessoas')
-                            fazerCadastro(restauranteCad, nomeReserva, data, horario, quantPessoas)
-
-            
+                        print('1. Atualizar Reserva')
+                        print('2. Voltar')
+                        resposta4 = int(input(''))
+                        limpa()
+                        if resposta4 == 1:
+                            limpa()
+                            restauranteAntigo = input("Digite o nome do restaurante cadastrado:\n")
+                            nomeReserva = input("Digite o nome da reserva cadastrada:\n")
+                            novoRestaurante = input("Digite o novo restaurante da reserva:\n")
+                            novoNome = input("Digite o novo nome da reserva:\n")
+                            novaData = input("Digite a nova data da reserva:\n")
+                            novoHorario = input("Digite o novo horário da reserva:\n")
+                            novaQuantPessoas = input("Digite a nova quantidade de pessoas:\n")
+                            atualizarReserva(restauranteAntigo,nomeReserva,novoRestaurante,novoNome,novaData,novoHorario,novaQuantPessoas)
+                        
             case 3:
                 print('Saindo...😁')
                 sleep(3)
